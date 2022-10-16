8ebb5097775cff644d8fe5c23e06c10cd3d7ecc7
Skye <callmeskye1405@gmail.com>
2022-10-16 16:59:53 +0200

    feat(raidpicker): added clear button and asigned choose button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { v4 as uuidv4 } from 'uuid';
 const LOCAL_STORAGE_KEY = 'raidApp.raids'
 
 function App() {
-  const [raids, addRaids] = useState([]);
+  const [raids, addRaids, showRaids] = useState([]);
   const raidRef = useRef();
 
   useEffect(() => {
     const storedRaids = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    addRaids(storedRaids)
+    addRaids(prevRaids => [...prevRaids,...storedRaids])
   }, [])
 
   useEffect(() =>{
@@ -26,12 +26,29 @@ function App() {
     raidRef.current.value = null
   }
 
+  function handleChooseRaid(){
+      addRaids(prevRaids =>{
+        return [raids[getRandomInt(raids.length)]]
+      }) 
+  }
+
+  function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+  }
+
+  function clearRaids(){
+    addRaids( prevRaids =>{
+      return []
+    })
+  }
+
   return (
     <>
     <Raidpicker raids = { raids } />
     <input ref={raidRef} type = 'text'></input>
     <button onClick={handleAddRaid}>add raid</button>
-    <button>chooseraid</button>
+    <button onClick={handleChooseRaid}>chooseraid</button>
+    <button onClick={clearRaids}>clearRaids</button>
     </>
   )
 }
