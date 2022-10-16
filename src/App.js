c92ae90f6a22b424d8c1ab654d3e98d1a3e597e1
Skye <callmeskye1405@gmail.com>
2022-10-16 16:10:02 +0200

    feat(raidpicker): raidpicker component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useRef, useEffect} from "react";
+import Raidpicker from "./Raidpicker";
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'raidApp.raids'
 
 function App() {
+  const [raids, addRaids] = useState([]);
+  const raidRef = useRef();
+
+  useEffect(() => {
+    const storedRaids = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    addRaids(storedRaids)
+  }, [])
+
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(raids))
+  }, [raids])
+
+  function handleAddRaid(e) {
+    const raid = raidRef.current.value
+    if( raid === '') return console.error('invalid Name');
+    addRaids( prevRaids =>{
+      return [...prevRaids, {id: uuidv4(), name: raid }]
+    })
+    raidRef.current.value = null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Raidpicker raids = { raids } />
+    <input ref={raidRef} type = 'text'></input>
+    <button onClick={handleAddRaid}>add raid</button>
+    <button>chooseraid</button>
+    </>
+  )
 }
 
 export default App;
